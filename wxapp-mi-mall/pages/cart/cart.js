2471d688efd3a4ef2dc14d10eb5a3adb614a680c
JoeWrights <1035208578@qq.com>
2018-06-06 23:55:30 +0800

    购物车页面完成

--- wxapp-mi-mall/pages/cart/cart.js
+++ wxapp-mi-mall/pages/cart/cart.js
@@ -1,11 +1,16 @@
 // pages/cart/cart.js
+const app=getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    // select_version:"",
+    // select_color:"",
+    // select_num:"",
+    cart_list:[]
   },
   goIndex(){
     wx.switchTab({
@@ -16,7 +21,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
+    
   },
 
   /**
@@ -30,7 +36,22 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    const select_color=wx.getStorageSync('color');
+    const select_version=wx.getStorageSync('version');
+    const select_num=wx.getStorageSync('select_num');
+    // const temp={version:select_version,color:select_color,num:select_num};
+    const attr_item=wx.getStorageSync('attr_item');
+    console.log(attr_item)
+    // return
+    let arr=this.data.cart_list;
+    // arr = [attr_item, ...arr]
+    arr=[...attr_item];
+    // arr.push(temp);
+    console.log(arr);
+    // console.log(temp)
+    this.setData({
+      cart_list:arr
+    });
   },
 
   /**
